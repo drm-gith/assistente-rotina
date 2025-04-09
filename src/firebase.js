9e0aebea33c6148f1137e275514f37c193edf9fc
drm-gith <drm4net@pm.me>
2025-04-09 19:08:00 -0300

    fix: correct storageBucket value in Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,19 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC3NMJbeW91PQ9ICKN5B0VepaH-hKnZpjY",
+
   authDomain: "assistente-rotina-3de83.firebaseapp.com",
+
   projectId: "assistente-rotina-3de83",
-  storageBucket: "assistente-rotina-3de83.appspot.com", // Fixed this value
+
+  storageBucket: "assistente-rotina-3de83.firebasestorage.app",
+
   messagingSenderId: "917619309359",
+
   appId: "1:917619309359:web:36c5d11c12a16dedf4ffda",
+
   measurementId: "G-7474GG9GV1"
+
 };
 
 // Initialize Firebase
