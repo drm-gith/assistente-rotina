37d7d11a2e89e102ca072613a15c4596879174d5
drm-gith <drm4net@pm.me>
2025-04-09 18:17:29 -0300

    refactor: reorganize Firebase imports and configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyC3NMJbeW91PQ9ICKN5B0VepaH-hKnZpjY',
-  authDomain: 'assistente-rotina-3de83.firebaseapp.com',
-  projectId: 'assistente-rotina-3de83',
-  storageBucket: 'assistente-rotina-3de83.appspot.com',
-  messagingSenderId: '917619309359',
-  appId: '1:917619309359:web:36c5d11c12a16dedf4ffda'
+  apiKey: "AIzaSyC3NMJbeW91PQ9ICKN5B0VepaH-hKnZpjY",
+  authDomain: "assistente-rotina-3de83.firebaseapp.com",
+  projectId: "assistente-rotina-3de83",
+  storageBucket: "assistente-rotina-3de83.firebasestorage.app",
+  messagingSenderId: "917619309359",
+  appId: "1:917619309359:web:36c5d11c12a16dedf4ffda",
+  measurementId: "G-7474GG9GV1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
